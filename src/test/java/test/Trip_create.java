3bfb9e2ad9b5c6e4cbdc66bdaa9649f64d17391b
chetan patil <patilchetan256@gmail.com>
2025-04-07 21:21:16 +0530

    TestNG_add_new

--- src/test/java/test/Trip_create.java
+++ src/test/java/test/Trip_create.java
@@ -9,6 +9,7 @@ import web.File_upload;
 public class Trip_create {
   @Test
   public void trip() throws IOException, InterruptedException {
+	  System.out.println("Started");
 	  File_upload.web();
   }
 }
